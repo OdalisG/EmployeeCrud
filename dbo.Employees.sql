7602959a1e43e74e1174b56a43cac5dfafcb22fe
Chica502 <oxg101@aol.com>
2018-06-06 17:52:14 -0400

    initial commit

---
+++ dbo.Employees.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeID] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (50)  NOT NULL,
+    [Location]   NVARCHAR (50)  NULL,
+    [Position]   NVARCHAR (50)  NOT NULL,
+    [EmailID]    NVARCHAR (100) NULL,
+    [Notes] NVARCHAR(50) NOT NULL, 
+    PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
+);
+
